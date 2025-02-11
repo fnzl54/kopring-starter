8665b3378860a52c56478aee458b78f81dac16dd
fnzl54 <rnjscksdud1@naver.com>
2025-02-11 16:08:18 +0900

    refactor: UserRepository Querydsl 수정

--- src/main/kotlin/com/group/libraryapp/domain/user/UserRepository.kt
+++ src/main/kotlin/com/group/libraryapp/domain/user/UserRepository.kt
@@ -1,11 +1,7 @@
 package com.group.libraryapp.domain.user
 
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Query
 
-interface UserRepository : JpaRepository<User, Long> {
+interface UserRepository : JpaRepository<User, Long>, UserRepositoryCustom {
     fun findByName(name: String): User?
-
-    @Query("SELECT u FROM User u LEFT JOIN fetch u.userLoanHistories")
-    fun findAllWithHistories(): List<User>
 }
